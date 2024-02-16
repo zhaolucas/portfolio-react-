d05d0582c07481074e72a501440af67c97d21dbb
Lucas Zhao <Lucaszhaopeiqi@hotmail.com>
2024-02-16 15:32:12 +0000

    fixed images not displaying

--- my-portfolio/src/Components/Project.js
+++ my-portfolio/src/Components/Project.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
-import styles from './Project.css'; 
+import './Project.css'; // Adjusted to simply import the CSS for side effects
 
 function Project({ project }) {
+  // Prepend process.env.PUBLIC_URL to the image path
+  const imageUrl = process.env.PUBLIC_URL + project.image;
+
   return (
-    <Card className="h-100"> {/* Ensures that the card takes full height */}
-      <Card.Img variant="top" src={project.image} className="img-fluid" style={{ objectFit: 'cover', height: '200px' }} />
-      <Card.Body className="d-flex flex-column"> {/* Flex container for the card body */}
+    <Card className="h-100">
+      <Card.Img variant="top" src={imageUrl} className="img-fluid" style={{ objectFit: 'cover', height: '200px' }} />
+      <Card.Body className="d-flex flex-column">
         <Card.Title>{project.title}</Card.Title>
-        {/* Flex-grow-1 will push everything else to the bottom */}
         <div className="mt-auto">
           <a href={project.deployedLink} target="_blank" rel="noopener noreferrer" className="btn btn-primary me-2">View Project</a>
           <a href={project.repoLink} target="_blank" rel="noopener noreferrer" className="btn btn-secondary">GitHub Repo</a>
@@ -17,4 +19,5 @@ function Project({ project }) {
     </Card>
   );
 }
+
 export default Project;
--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -3,20 +3,24 @@ import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 import './Home.css';
 
 function Home() {
+  // Correctly constructing the image URL
+  const mugshotImageUrl = process.env.PUBLIC_URL + '/images/mugshot.jpg';
+
   return (
     <Container className="mt-5">
       <Row className="justify-content-center">
         <Col md={8}>
           <Card className="text-center">
             <Card.Header as="h1">Lucas Zhao</Card.Header>
-            <Card.Img variant="top" src="/images/mugshot.jpg" alt="Lucas Zhao" className="custom-mugshot" />
+            {/* Use mugshotImageUrl for the src attribute */}
+            <Card.Img variant="top" src={mugshotImageUrl} alt="Lucas Zhao" className="custom-mugshot" />
             <Card.Body>
               <Card.Title>Hello, World</Card.Title>
               <Card.Text>
                 Check out my portfolio of projects in the project gallery.
               </Card.Text>
-              <Button variant="primary" href="/cv.pdf" download="LucasZhao_CV.pdf">Download CV</Button>
-
+              {/* Adjust href for CV download */}
+              <Button variant="primary" href={`${process.env.PUBLIC_URL}/cv.pdf`} download="LucasZhao_CV.pdf">Download CV</Button>
             </Card.Body>
           </Card>
         </Col>
